bfe6fd3d232fa22b7f6f29f28db6884d567907bf
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-02-11 14:52:47 +0200

    022

---
+++ task4/ArrayList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class ArrayList
+    {
+        private object[] array;
+        public object this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                    return array[index];
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public int Count => array.Length;
+        public ArrayList()
+        {
+            array = new object[0];
+        }
+
+        public void Add(object item)
+        {
+            object[] temp = new object[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+                temp[i] = array[i];
+            temp[array.Length] = item;
+            array = temp;
+        }
+        public void Remove(object item)
+        {
+            int n = -1, j = 0;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i].GetHashCode() == item.GetHashCode())
+                    n = i;
+
+            if (n == -1)
+                return;
+
+            object[] temp = new object[array.Length - 1];
+            for (int i = 0; i < array.Length; i++)
+                if (i != n)
+                {
+                    temp[j] = array[i];
+                    j++;
+                }
+
+            array = temp;
+        }
+        public void Clear()
+        {
+            array = new object[0];
+        }
+    }
+}
---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример с урока 022 с собственным классом ArrayList
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(1);
+            arrayList.Add((object)2);
+            arrayList.Add(new Random());
+
+            int i1 = (int)arrayList[0];
+
+            //for(int i = 0; i < arrayList.Count; i++)
+            //    Console.WriteLine((int)arrayList[i]); //ошибка
+
+            for (int i = 0; i < arrayList.Count; i++)
+                Console.WriteLine(arrayList[i]);
+        }
+    }
+}
